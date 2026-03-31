f5645871ba5e39fe094841805c41fda4d936c205
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-03-31 10:35:51 -0300

    fix: resolve 500 error by adding @Table annotation and H2 config

--- api/src/main/java/com/library/api/model/Livro.java
+++ api/src/main/java/com/library/api/model/Livro.java
@@ -5,6 +5,7 @@ import lombok.Data; // Importa o Lombok para evitar criar códigos repetitivos (
 
 @Entity // Avisa ao Spring que esta classe será uma tabela no Banco de Dados
 @Data // Gera automaticamente os métodos de leitura e escrita (getters e setters)
+@Table(name = "tb_livros")
 public class Livro {
     @Id  // Define que este campo é a Chave Primária (ID único)
     @GeneratedValue(strategy = GenerationType.IDENTITY) // O banco de dados gera o número (1, 2, 3...) automaticamente
